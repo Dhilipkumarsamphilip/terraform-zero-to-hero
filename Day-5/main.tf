d73e5dbf43a1e8dfc4357d05054ad9c26ed4b48b
DHILIPKUMARSAMPHILIP <94403996+Dhilipkumarsamphilip@users.noreply.github.com>
2025-06-17 22:33:32 +0530

    Update main.tf

--- Day-5/main.tf
+++ Day-5/main.tf
@@ -1,6 +1,5 @@
-# Define the AWS provider configuration.
 provider "aws" {
-  region = "us-east-1"  # Replace with your desired AWS region.
+  region = "us-east-1"
 }
 
 variable "cidr" {
@@ -8,8 +7,8 @@ variable "cidr" {
 }
 
 resource "aws_key_pair" "example" {
-  key_name   = "terraform-demo-abhi"  # Replace with your desired key name
-  public_key = file("~/.ssh/id_rsa.pub")  # Replace with the path to your public key file
+  key_name = "terra_task"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
 
 resource "aws_vpc" "myvpc" {
@@ -17,9 +16,9 @@ resource "aws_vpc" "myvpc" {
 }
 
 resource "aws_subnet" "sub1" {
-  vpc_id                  = aws_vpc.myvpc.id
-  cidr_block              = "10.0.0.0/24"
-  availability_zone       = "us-east-1a"
+  vpc_id = aws_vpc.myvpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
   map_public_ip_on_launch = true
 }
 
@@ -36,71 +35,68 @@ resource "aws_route_table" "RT" {
   }
 }
 
-resource "aws_route_table_association" "rta1" {
-  subnet_id      = aws_subnet.sub1.id
+resource "aws_route_table_association" "RT1" {
+  subnet_id = aws_subnet.sub1.id
   route_table_id = aws_route_table.RT.id
 }
 
-resource "aws_security_group" "webSg" {
-  name   = "web"
+resource "aws_security_group" "asg" {
+  name = "web_security"
   vpc_id = aws_vpc.myvpc.id
 
   ingress {
-    description = "HTTP from VPC"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    description = "HTTP from vpc"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
+  ingress{
     description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+  egress{
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name = "Web-sg"
+    Name = "asg"
   }
 }
 
 resource "aws_instance" "server" {
-  ami                    = "ami-0261755bbcb8c4a84"
-  instance_type          = "t2.micro"
-  key_name      = aws_key_pair.example.key_name
-  vpc_security_group_ids = [aws_security_group.webSg.id]
-  subnet_id              = aws_subnet.sub1.id
+  ami = "ami-084568db4383264d4"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.example.key_name
+  vpc_security_group_ids = [aws_security_group.asg.id]
+  subnet_id = aws_subnet.sub1.id
+  associate_public_ip_address = true
 
   connection {
-    type        = "ssh"
-    user        = "ubuntu"  # Replace with the appropriate username for your EC2 instance
-    private_key = file("~/.ssh/id_rsa")  # Replace with the path to your private key
-    host        = self.public_ip
+    type = "ssh"
+    user = "ubuntu"
+    private_key = file("~/.ssh/id_rsa")
+    host = self.public_ip
   }
 
-  # File provisioner to copy a file from local to the remote EC2 instance
   provisioner "file" {
-    source      = "app.py"  # Replace with the path to your local file
-    destination = "/home/ubuntu/app.py"  # Replace with the path on the remote instance
+    source = "server.py"
+    destination = "/home/ubuntu/server.py"
   }
 
   provisioner "remote-exec" {
-    inline = [
-      "echo 'Hello from the remote instance'",
-      "sudo apt update -y",  # Update package lists (for ubuntu)
-      "sudo apt-get install -y python3-pip",  # Example package installation
-      "cd /home/ubuntu",
-      "sudo pip3 install flask",
-      "sudo python3 app.py &",
+  inline = [ 
+    "sudo apt-get update -y",
+    "sudo apt-get install -y python3",
+    "cd /home/ubuntu",
+    "sudo nohup python3 server.py > server.log 2>&1 &"
     ]
   }
 }
-
